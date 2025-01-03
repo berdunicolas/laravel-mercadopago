f9869518142c673a00306760e91f7ee8014919c1
Nicolas <berdunicolas@gmail.com>
2025-01-03 12:35:09 -0300

    Feat integracion minima de mercado pago

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\PaymentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cobro', [PaymentController::class, 'pantallaDeCobro']);
+
+Route::get('/pago', [PaymentController::class, 'formularioDePago']);
+
+Route::post('/process_payment', [PaymentController::class, 'procesoDePago']);
